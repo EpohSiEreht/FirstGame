868e93e8a6846e41c5cb4dd4bb83a5a5c6875973
David Lee <ddlee.907@gmail.com>
2015-11-24 12:49:30 -0500

    add ending restart button and remove alert function

--- Game Folder/script/scripts.js
+++ Game Folder/script/scripts.js
@@ -2,12 +2,14 @@ console.log('testing 123')
 // Create ability to play music at 50% volume
 function getStarted() {
   var musicB = document.getElementById('battlemusic');
-  musicB.setAttribute('src', 'music/battlemusic.mp3');
   musicB.volume=0.5;
   var instructionsText = $('#instructions');
   instructionsText.on('click', function() {
     this.remove();
   });
+}
+function hidingEnding() {
+  $('#ending').attr('type', 'display: block');
 }
 
 // Create score counter for rock, paper, scissors
@@ -19,19 +21,18 @@ var $computerScoreCount = $('#computerScore');
 // Create variables that'll hold the value chosen the user and computer
 var $playerChoice = $('#playerChoice');
 var $computerChoice = $('#computerChoice');
-
+// Create variables that will hold the monsters level status
 var monsterLevel = {
   water: 1,
   fire: 1,
   grass: 1
 }
-
 var computerMonsterLevel = {
   water: 1,
   fire: 1,
   grass: 1
 }
-
+// Create a variable to detect who won the round
 var whoWon;
 // Create a function that'll compare chosen values to determine winner
 function compare(choice1, choice2) {
@@ -82,6 +83,8 @@ function compare(choice1, choice2) {
       return updateScores();
   }
 }
+
+
 // Create the ability to update scores while evolving monsters
 function updateScores() {
   // Holds the current score value and display on the web page
@@ -93,7 +96,11 @@ function updateScores() {
   monsterLevel[$playerChoice.text()];
   computerMonsterLevel[$computerChoice.text()];
 
-  // Evovle player's monsters per point
+  //////////////////////////////////////////
+  //////////////////////////////////////////
+  // Evolve player's monsters per point //
+  //////////////////////////////////////////
+  //////////////////////////////////////////
 
   if(whoWon === 1 && monsterLevel[$playerChoice.text()] === 3) {
     $('#'+$playerChoice.text()+'A').attr('src', "images/elements/"+$playerChoice.text()+"4.gif");
@@ -101,55 +108,66 @@ function updateScores() {
     monsterLevel[$playerChoice.text()]++;
     $('#levelup').attr('src', 'music/levelup.mp3');
   }
-  if(whoWon === 1 && monsterLevel[$playerChoice.text()] === 2) {
+  else if(whoWon === 1 && monsterLevel[$playerChoice.text()] === 2) {
     $('#'+$playerChoice.text()+'A').attr('src', "images/elements/"+$playerChoice.text()+"3.gif");
     $('#'+$playerChoice.text()+'A').attr('width', "130px");
     monsterLevel[$playerChoice.text()]++;
     $('#levelup').attr('src', 'music/levelup.mp3');
   }
-  if(whoWon === 1 && monsterLevel[$playerChoice.text()] === 1) {
+  else if(whoWon === 1 && monsterLevel[$playerChoice.text()] === 1) {
     $('#'+$playerChoice.text()+'A').attr('src', "images/elements/"+$playerChoice.text()+"2.gif");
     $('#'+$playerChoice.text()+'A').attr('width', "100px");
     monsterLevel[$playerChoice.text()]++;
     $('#levelup').attr('src', 'music/levelup.mp3');
   }
-  else if(playerScore === 4) {
+  if(whoWon === 1 & playerScore === 4) {
     $('#levelup').attr('src', 'music/levelup.mp3');
   }
   else if(whoWon === 1 && playerScore === 5 && computerScore < 5) {
-    alert("You won the game!!! Thank you for playing! Click OK to restart the game.");
-
-    // Make sure to refresh page after the game has finished
-    location.reload();
+    $('#levelup').attr('src', 'music/levelup.mp3');
+    $('#battlemusic').attr('src', 'music/endingSong.mp3');
+    $('#critical').attr('src', '');
+    $('#ending').css("display", "block");
+    $('#ending h5').text('You won!!!! Hope you enjoyed my first game and thank you for playing!.')
   }
 
-  // Evolve computer's monsters per point
+  //////////////////////////////////////////
+  //////////////////////////////////////////
+  // Evolve computer's monsters per point //
+  //////////////////////////////////////////
+  //////////////////////////////////////////
   if(whoWon === 2 && computerMonsterLevel[$computerChoice.text()] === 3) {
     $('#'+$computerChoice.text()+'Z').attr('src', "images/computerMonster/"+$computerChoice.text()+"4.gif");
-    $('#'+$computerChoice.text()+'Z').attr('width', "150px");
+    $('#'+$computerChoice.text()+'Z').attr('width', "120px");
     computerMonsterLevel[$computerChoice.text()]++;
-    $('#levelup').attr('src', 'music/levelup.mp3');
+    $('#attack').attr('src', 'music/attack.mp3');
   }
 
-  if(whoWon === 2 && computerMonsterLevel[$computerChoice.text()] === 2) {
+  else if(whoWon === 2 && computerMonsterLevel[$computerChoice.text()] === 2) {
     $('#'+$computerChoice.text()+'Z').attr('src', "images/computerMonster/"+$computerChoice.text()+"3.gif");
     $('#'+$computerChoice.text()+'Z').attr('width', "110px");
     computerMonsterLevel[$computerChoice.text()]++;
-    $('#levelup').attr('src', 'music/levelup.mp3');
+    $('#attack').attr('src', 'music/attack.mp3');
   }
 
-  if(whoWon === 2 && computerMonsterLevel[$computerChoice.text()] === 1) {
+  else if(whoWon === 2 && computerMonsterLevel[$computerChoice.text()] === 1) {
     $('#'+$computerChoice.text()+'Z').attr('src', "images/computerMonster/"+$computerChoice.text()+"2.gif");
     $('#'+$computerChoice.text()+'Z').attr('width', "100px");
     computerMonsterLevel[$computerChoice.text()]++;
-    $('#levelup').attr('src', 'music/levelup.mp3');
+    $('#attack').attr('src', 'music/attack.mp3');
   }
-  else if(computerScore === 4) {
-    $('#levelup').attr('src', 'music/levelup.mp3');
+  if(whoWon === 2 && computerScore === 4) {
+    $('#critical').attr('src', 'music/aboutToLose.mp3');
+    $('#attack').attr('src', 'music/attack.mp3');
   }
   else if(whoWon = 2 && computerScore === 5 && playerScore < 5) {
-    alert("You lost... Better luck next time. Click OK to restart the game.");
-    // Make sure to refresh the page after the game has finished
-    location.reload();
+    $('#attack').attr('src', 'music/attack.mp3');
+    $('#ending').css("display", "block");
+    $('#ending h5').text('You lost!!!! Try again by clicking okay!')
   }
+
 }
+
+$('button').on("click", function() {
+  location.reload();
+})
